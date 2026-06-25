4a4855f0091f8d0c2a7b45fc78e9c82b577ed98f
Dillon Mahmoudi <dillon.mahmoudi@gmail.com>
2026-06-25 17:07:17 +0000

    feat: switch startup citation to Chicago author-date full-name style

--- R/zzz.R
+++ R/zzz.R
@@ -3,14 +3,15 @@
 # ----------------------------------------------------------------------
 
 .onAttach <- function(libname, pkgname) {
+  ver <- as.character(utils::packageVersion("lehdr"))
   packageStartupMessage(
-    "lehdr ", utils::packageVersion("lehdr"), "\n",
+    "lehdr ", ver, "\n",
     "\n",
-    "To cite lehdr in published work:\n",
+    "To cite lehdr in published work (Chicago author-date):\n",
     "\n",
-    "  Green, J., Wang, L., & Mahmoudi, D. (2025). lehdr: Grab\n",
-    "  Longitudinal Employer-Household Dynamics (LEHD) Flat Files\n",
-    "  (R package version ", utils::packageVersion("lehdr"), ").\n",
+    "  Green, Jamaal, Liming Wang, and Dillon Mahmoudi. 2025.\n",
+    "  \"lehdr: Grab Longitudinal Employer-Household Dynamics\n",
+    "  (LEHD) Flat Files.\" R package version ", ver, ".\n",
     "  https://github.com/jamgreen/lehdr/\n",
     "\n",
     "Run citation(\"lehdr\") for a BibTeX entry.\n",
